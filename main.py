9d29f422263e6791b783467c135f56890f16f812
Sakti Suman Tripathy <65999640+SaktiSuman@users.noreply.github.com>
2022-01-04 18:44:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+p = float(input("Enter the principle: "))
+r = int(input("Enter the rate of interest: "))
+t = float(input("Enter the time period in years: "))
+si = (p * r * t)/ 100
+print("The simple interest is: ", si)
